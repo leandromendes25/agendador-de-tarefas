fefe54faff02eefdc9bec2a447df0f72f0d68e93
leandromendes25 <leandromiendes@hotmail.com>
2025-12-30 18:59:22 -0300

    commit inicial

---
+++ src/main/java/com/leandromendes25/agendador_de_tarefas/AgendadorDeTarefasApplication.java
@@ -0,0 +1,13 @@
+package com.leandromendes25.agendador_de_tarefas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorDeTarefasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorDeTarefasApplication.class, args);
+	}
+
+}
