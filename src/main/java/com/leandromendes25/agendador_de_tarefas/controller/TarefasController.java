dd7e934bc1fa0a9d05403b85ff866d7ca595f0a4
leandromendes25 <leandromiendes@hotmail.com>
2026-01-13 11:46:25 -0300

    adicionados metodos de put, patch e delete de tarefas

--- src/main/java/com/leandromendes25/agendador_de_tarefas/controller/TarefasController.java
+++ src/main/java/com/leandromendes25/agendador_de_tarefas/controller/TarefasController.java
@@ -2,6 +2,7 @@ package com.leandromendes25.agendador_de_tarefas.controller;
 
 import com.leandromendes25.agendador_de_tarefas.business.TarefasService;
 import com.leandromendes25.agendador_de_tarefas.business.dto.TarefasDTO;
+import com.leandromendes25.agendador_de_tarefas.infrastructure.enums.StatusNotificacaoEnum;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
@@ -31,5 +32,21 @@ public class TarefasController {
  @GetMapping
     public ResponseEntity<List<TarefasDTO>> buscaTarefasEmail(@RequestHeader("Authorization") String token){
         return ResponseEntity.ok(tarefasService.buscaTarefasPorEmail(token));
+ }
+ @DeleteMapping
+    public ResponseEntity<Void> deletaTarefaPorId(@RequestParam("id") String id){
+        tarefasService.deletaTarefaPorId(id);
+        return ResponseEntity.noContent().build();
+ }
+ @PutMapping
+ public ResponseEntity<TarefasDTO> updateDeTarefas(@RequestBody TarefasDTO dto, @RequestParam("id") String id){
+     return ResponseEntity.ok(tarefasService.updateTarefas(dto, id));
+
+ }
+ @PatchMapping
+    public ResponseEntity<TarefasDTO> alteraStatusNotificacao(
+            @RequestParam("status")StatusNotificacaoEnum status,
+            @RequestParam("id") String id){
+    return ResponseEntity.ok(tarefasService.alteraStatus(status, id));
  }
 }
